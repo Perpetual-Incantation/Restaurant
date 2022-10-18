2a029c68aeff8c91ad95659975337d4183899e79
Perpetual-Incantation <snehajain93106@gmail.com>
2022-10-18 22:56:29 +0530

    rnw

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Restaurant from "./component/basics/Restaurant";
 //import UseState from './component/Hooks/UseState'
 //import UseEffect from './component/Hooks/UseEffect'
 //import UseReducer from './component/Hooks/useReducer';
+import styles from './component/basics/styles.css'
 const App = () => {
-  return <Restaurant />;
+  return (
+  <div>
+  <h1 align="center">Restaurant Website</h1>
+  <Restaurant />;
+
+  </div>
+  )
 };
 
 
